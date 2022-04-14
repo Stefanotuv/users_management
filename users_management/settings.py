d44632ea516cb9f616347074a3a92583482bb3ff
stefanotuv <ste.tuveri@gmail.com>
2022-04-14 23:41:45 +0100

    modified settings for environment

--- users_management/settings.py
+++ users_management/settings.py
@@ -33,17 +33,17 @@ import cx_Oracle
 
 BASE_DIR = Path(__file__).resolve().parent.parent
 
-if os.environ['ENVIRONMENT'] =='prod':
-    cx_Oracle.init_oracle_client(lib_dir="/Users/stefano/Dropbox/NewDev/user_management_connect")
-else:
-    cx_Oracle.init_oracle_client(lib_dir="/Users/stefano/Dropbox/NewDev/user_management_connect")
+
 
 dotenv_file = os.path.join(BASE_DIR, ".env")
 if os.path.isfile(dotenv_file):
     dotenv.load_dotenv(dotenv_file)
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/4.0/howto/deployment/checklist/
-
+if os.environ['ENVIRONMENT'] =='prod':
+    cx_Oracle.init_oracle_client(lib_dir="/home/ubuntu/project_grouping/connect/instantclient_21_5")
+else:
+    cx_Oracle.init_oracle_client(lib_dir="/Users/stefano/Dropbox/NewDev/user_management_connect")
 # SECURITY WARNING: keep the secret key used in production secret!
 SECRET_KEY = os.environ['SECRET_KEY']
 
@@ -232,6 +232,9 @@ LOGIN_URL = 'users_login'
 # added
 MEDIA_ROOT = os.path.join(BASE_DIR, 'users','media') # MEDIA_ROOT = BASE_DIR
 MEDIA_URL = '/users/media/'
+
+# added for prod to launch the collect static command
+STATIC_ROOT = os.path.join(BASE_DIR, 'static/')
 
 
 
