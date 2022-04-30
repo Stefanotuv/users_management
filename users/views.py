55a0ecd6eb0d6b685e1c4cd1e0792f403b8e37c5
stefanotuv <ste.tuveri@gmail.com>
2022-04-30 23:54:02 +0100

    changes to settings to work on dev and prod DB to make testing easier

--- users/views.py
+++ users/views.py
@@ -289,8 +289,8 @@ class UserProfileChangePictureView(UpdateView):
         # rdir = "users/media/profile_pics/gallery" # needed in dev (for MAC?)
         rdir = os.path.join(settings.MEDIA_ROOT,"profile_pics/gallery") # needed in production
         files = os.listdir(rdir)
-        files_path = [os.path.join(rdir, fil) for fil in files]
-        mylist = ['/grouping/'+f for f in files_path]
+        files_path = [os.path.join("grouping/media/profile_pics/gallery",fil) for fil in files]
+        mylist = ['/'+f for f in files_path]
         # # context = self.get_context_data(**kwargs)
         context = {'mylist': mylist}
 
